d0017cb467c798a2caae3fa8b0b8b5b23b60569c
RuneXa <richiebachtiar@gmail.com>
2019-01-30 09:55:49 +0700

    Small fix to card Layout

--- src/core-ui/Card.js
+++ src/core-ui/Card.js
@@ -19,19 +19,19 @@ type Props = {
 
 export class Card extends Component<Props> {
   render() {
-    const {buttonBackColor, cardBackColor, textColor} = this.props.containerStyle;
+    const containerStyle = this.props.containerStyle;
+    const {cardBackColor, textColor, buttonBackColor} = containerStyle ? containerStyle : {};
+
     return (
       <View style={[styles.container, {backgroundColor: cardBackColor ? cardBackColor : 'white'}]}>
         <View style={styles.imageContainer}>
           <Image style={styles.image} source={{uri: this.props.imageUrl}} />
         </View>
         <View style={styles.bottomContainer}>
-          <Text style={{alignSelf: 'flex-start', color: textColor ? textColor : 'black'}}>
-            {this.props.text}
-          </Text>
+          <Text style={{flex: 1, color: textColor ? textColor : 'black'}}>{this.props.text}</Text>
           <Button
             style={{
-              alignSelf: 'flex-end',
+              flex: 1,
               backgroundColor: buttonBackColor ? buttonBackColor : 'white',
             }}
             title={this.props.buttonText}
@@ -48,6 +48,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'column',
+    borderColor: 'gray',
+    borderWidth: 5,
     margin: 20,
   },
   image: {
@@ -62,5 +64,7 @@ const styles = StyleSheet.create({
   bottomContainer: {
     padding: 10,
     flexDirection: 'row',
+    alignItems: 'stretch',
+    justifyContent: 'space-between',
   },
 });
